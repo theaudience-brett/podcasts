b8a12fd28ce7c91af077f01fe2f50985b5bb4b6b
Brett Mack <brett.mack@bauermedia.co.uk>
2012-08-02 00:53:20 +0100

    Fixed minor error

--- src/Foxie/PodcastsBundle/Controller/DefaultController.php
+++ src/Foxie/PodcastsBundle/Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    public function indexAction()
     {
         return $this->render('FoxiePodcastsBundle:Default:index.html.twig');
     }
